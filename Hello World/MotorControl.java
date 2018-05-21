0bf93310d9629ac65cecfaebd2a4dc032c375d7d
adhingra10 <31217124+adhingra10@users.noreply.github.com>
2018-05-21 09:33:22 -0500

    yes team we did not meet the milestone

--- Hello World/MotorControl.java
+++ Hello World/MotorControl.java
@@ -22,11 +22,12 @@ public class MotorControl
         LCD.drawString("BACKWARD", 0, 1);
         Button.waitForAnyPress();
         Motor.B.stop();
+        LCD.clear();
         
         Button.waitForAnyPress();
         
         LCD.drawString("Program 2", 0, 0);
-        Motor.B.setSpeed(2);
+        Motor.B.setSpeed(360 * 2);
         Motor.B.forward();
         Delay d = new Delay();
         d.msDelay(2000);
@@ -43,5 +44,40 @@ public class MotorControl
         Motor.B.stop();
         LCD.drawString(""+Motor.B.getTachoCount(),0,4);
         Button.waitForAnyPress();
+        LCD.clear();
+        
+        LCD.drawString("Program 3", 0, 0);
+        Button.waitForAnyPress();
+        Motor.B.rotate(360 * 4);
+        LCD.drawString(""+Motor.B.getTachoCount(),0,1);
+        Motor.B.rotateTo(0);
+        LCD.drawString(""+Motor.B.getTachoCount(),0,2);
+        Button.waitForAnyPress();
+        LCD.clear();
+        
+        LCD.drawString("Program 4", 0, 0);
+        Motor.B.resetTachoCount();
+        Motor.B.backward();
+        int tach2 = Motor.B.getTachoCount();
+        while((tach2 > -360 * 4))
+        {
+            tach2 = Motor.B.getTachoCount();
+            LCD.drawString("" + tach2, 0, 1);
+            if (Button.waitForAnyPress(1) != 0)
+            {
+                break;
+            }
+        }
+        LCD.drawString("" + Motor.B.getTachoCount(), 0, 1);
+        LCD.clear();
+        Button.waitForAnyPress();
+        
+        LCD.drawString("Program 5", 0, 0);
+        Motor.A.setSpeed(360 * 2);
+        Motor.B.setSpeed(360 * 2);
+        Motor.C.setSpeed(360 * 2);
+        
+        int startTime = elapsed();    
+        
     }
 }
