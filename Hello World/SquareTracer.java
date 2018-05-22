7472d61a5c6f5477ec1bc9dbddec9eb65c328aac
adhingra10 <31217124+adhingra10@users.noreply.github.com>
2018-05-22 09:30:28 -0500

    finished squaretracer

---
+++ Hello World/SquareTracer.java
@@ -0,0 +1,24 @@
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
+/**
+ * DRAW A SQUARE
+ * @author ERIC CHEN
+ */
+public class SquareTracer
+{
+    public static void square(int sideLength)
+    {
+        DifferentialPilot p = new DifferentialPilot (2.25f, 5.5f, Motor.A, Motor.C);
+        for (int i = 0; i < 4; i++)
+        {
+            p.travel(sideLength);
+            p.rotate(90);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        square(10);
+    }
+}
